576eae0812738d2267dafe2230555bb8ac0ea52e
ujwal16102006 <156877262+ujwal16102006@users.noreply.github.com>
2026-08-29 10:41:08 +0530

    Time: 1 ms (64.85%) | Memory: 42.2 MB (39.60%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.java
+++ 2507-number-of-common-factors/number-of-common-factors.java
@@ -1,7 +1,7 @@
 class Solution {
     public int commonFactors(int a, int b) {
         int c=0;
-        for(int i=1;i<=b;i++){
+        for(int i=1;i<=a && i<=b;i++){
             if(a%i==0 && b%i==0)
               {
                c++;
