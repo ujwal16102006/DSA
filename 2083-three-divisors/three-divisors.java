fe4ce99ffaa7b84d4c38aa11dae1ac12b793541d
ujwal16102006 <156877262+ujwal16102006@users.noreply.github.com>
2026-08-29 10:46:33 +0530

    Time: 1 ms (76.92%) | Memory: 42 MB (88.67%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isThree(int n) {
+        int c=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                c++;
+            }
+        }
+        if(c==3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
